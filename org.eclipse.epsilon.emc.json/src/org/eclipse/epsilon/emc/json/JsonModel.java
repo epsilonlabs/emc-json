db08dfdcfba5a9e5adc45d50cda837dbab12193d
Antonio García-Domínguez <a.garcia-dominguez@york.ac.uk>
2023-11-21 18:16:47 +0000

    Avoid deprecated APIs in Apache HttpClient

--- org.eclipse.epsilon.emc.json/src/org/eclipse/epsilon/emc/json/JsonModel.java
+++ org.eclipse.epsilon.emc.json/src/org/eclipse/epsilon/emc/json/JsonModel.java
@@ -1,18 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2022-2023 The University of York.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * Contributors:
+ *     Sina Madani - initial API and implementation
+ *     Antonio Garcia-Dominguez - complete implementation and add tests/docs
+ ******************************************************************************/
 package org.eclipse.epsilon.emc.json;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collection;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.eclipse.epsilon.common.util.FileUtil;
 import org.eclipse.epsilon.common.util.StringProperties;
 import org.eclipse.epsilon.emc.plainxml.PlainXmlModel;
@@ -33,60 +49,59 @@ public class JsonModel extends CachedModel<Object> {
 	public static final String PROPERTY_URI = "uri";
 	public static final String PROPERTY_USERNAME = "username";
 	public static final String PROPERTY_PASSWORD = "password";
-	
+
 	protected File file;
 	protected String uri;
 	protected String username;
 	protected String password;
 	protected Object root;
-	
+
 	public JsonModel() {
 		propertyGetter = new JsonPropertyGetter();
 	}
-	
+
 	public Object getRoot() {
 		return root;
 	}
-	
+
 	public void setRoot(Object root) {
 		this.root = root;
 	}
-	
+
 	public void setFile(File file) {
 		this.file = file;
 	}
-	
+
 	public File getFile() {
 		return file;
 	}
-	
+
 	public void setUri(String uri) {
 		this.uri = uri;
 	}
-	
+
 	public String getUri() {
 		return uri;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
 
 	@Override
-	public Object getEnumerationValue(String enumeration, String label)
-			throws EolEnumerationValueNotFoundException {
+	public Object getEnumerationValue(String enumeration, String label) throws EolEnumerationValueNotFoundException {
 		throw new UnsupportedOperationException();
 	}
 
@@ -112,8 +127,7 @@ public class JsonModel extends CachedModel<Object> {
 
 	@Override
 	public boolean owns(Object instance) {
-		return instance instanceof JSONObject /*||
-			instance instanceof JSONArray*/;
+		return instance instanceof JSONObject || instance instanceof JSONArray;
 	}
 
 	@Override
@@ -123,8 +137,7 @@ public class JsonModel extends CachedModel<Object> {
 
 	@Override
 	public boolean hasType(String type) {
-		return JSONObject.class.getSimpleName().equals(type) || 
-			JSONArray.class.getSimpleName().equals(type);
+		return JSONObject.class.getSimpleName().equals(type) || JSONArray.class.getSimpleName().equals(type);
 	}
 
 	@Override
@@ -141,8 +154,7 @@ public class JsonModel extends CachedModel<Object> {
 	public boolean store() {
 		if (file != null) {
 			return store(file.getAbsolutePath());
-		}
-		else {
+		} else {
 			throw new UnsupportedOperationException();
 		}
 	}
@@ -154,76 +166,80 @@ public class JsonModel extends CachedModel<Object> {
 	}
 
 	@Override
-	protected Collection<Object> getAllOfTypeFromModel(String type)
-			throws EolModelElementTypeNotFoundException {
+	protected Collection<Object> getAllOfTypeFromModel(String type) throws EolModelElementTypeNotFoundException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected Collection<Object> getAllOfKindFromModel(String kind)
-			throws EolModelElementTypeNotFoundException {
+	protected Collection<Object> getAllOfKindFromModel(String kind) throws EolModelElementTypeNotFoundException {
 		return getAllOfTypeFromModel(kind);
 	}
 
 	@Override
 	protected Object createInstanceInModel(String type)
-			throws EolModelElementTypeNotFoundException,
-			EolNotInstantiableModelElementTypeException {
-		
+			throws EolModelElementTypeNotFoundException, EolNotInstantiableModelElementTypeException {
+
 		if (JSONObject.class.getSimpleName().equals(type)) {
 			return new JSONObject();
-		}
-		else if (JSONArray.class.getSimpleName().equals(type)) {
+		} else if (JSONArray.class.getSimpleName().equals(type)) {
 			return new JSONArray();
 		}
 		throw new EolModelElementTypeNotFoundException(this.getName(), type);
 	}
 
 	@Override
-	public void load(StringProperties properties, IRelativePathResolver resolver)
-			throws EolModelLoadingException {
-		
+	public void load(StringProperties properties, IRelativePathResolver resolver) throws EolModelLoadingException {
 		super.load(properties, resolver);
-		
+
 		String filePath = properties.getProperty(PlainXmlModel.PROPERTY_FILE);
-		
+
 		if (filePath != null && filePath.trim().length() > 0) {
 			file = new File(resolver.resolve(filePath));
-		}
-		else {
+		} else {
 			uri = properties.getProperty(JsonModel.PROPERTY_URI);
 			username = properties.getProperty(JsonModel.PROPERTY_USERNAME);
 			password = properties.getProperty(JsonModel.PROPERTY_PASSWORD);
 		}
-		
+
 		load();
 	}
-	
+
 	@Override
 	protected void loadModel() throws EolModelLoadingException {
+		if (!readOnLoad)
+			return;
+
 		try {
-			if (readOnLoad) {
-				Reader reader = null;
-				if (file == null) {
-					HttpClient httpClient = new DefaultHttpClient();
+			if (uri != null) {
+				URI parsedUri = new URI(uri);
+
+				HttpClientBuilder builder = HttpClients.custom();
+				if (username != null) {
+					BasicCredentialsProvider creds = new BasicCredentialsProvider();
+					creds.setCredentials(new AuthScope(parsedUri.getHost(), parsedUri.getPort()),
+							new UsernamePasswordCredentials(username, password));
+
+					builder.setDefaultCredentialsProvider(creds);
+				}
+
+				try (CloseableHttpClient httpClient = builder.build()) {
 					HttpGet httpGet = new HttpGet(uri);
-					if (username != null) {
-						httpGet.addHeader(BasicScheme.authenticate(
-								 new UsernamePasswordCredentials(username, password),
-								 "UTF-8", false));
-					}
+
 					HttpResponse httpResponse = httpClient.execute(httpGet);
 					HttpEntity responseEntity = httpResponse.getEntity();
-					reader = new InputStreamReader(responseEntity.getContent());
+
+					Reader reader = new InputStreamReader(responseEntity.getContent(), StandardCharsets.UTF_8);
+					root = JSONValue.parse(reader);
 				}
-				else {
-					reader = new FileReader(file);
+			} else if (file != null) {
+				try (Reader reader = new FileReader(file, StandardCharsets.UTF_8)) {
+					root = JSONValue.parse(reader);
 				}
-				root = JSONValue.parse(reader);
+			} else {
+				throw new IllegalStateException("Neither URI nor file path have been set");
 			}
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
 			throw new EolModelLoadingException(ex, this);
 		}
 	}
@@ -232,16 +248,14 @@ public class JsonModel extends CachedModel<Object> {
 	protected void disposeModel() {
 		root = null;
 	}
-	
+
 	@Override
-	protected boolean deleteElementInModel(Object instance)
-			throws EolRuntimeException {
+	protected boolean deleteElementInModel(Object instance) throws EolRuntimeException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	protected Object getCacheKeyForType(String type)
-			throws EolModelElementTypeNotFoundException {
+	protected Object getCacheKeyForType(String type) throws EolModelElementTypeNotFoundException {
 		return null;
 	}
 
@@ -249,7 +263,7 @@ public class JsonModel extends CachedModel<Object> {
 	protected Collection<String> getAllTypeNamesOf(Object instance) {
 		return Arrays.asList(instance.getClass().getSimpleName());
 	}
-	
+
 	public boolean isLoaded() {
 		return root != null;
 	}
